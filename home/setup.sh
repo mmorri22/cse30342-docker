98c26594da117757e1b084f7e83a64161e711383
mmorri22 <48807588+mmorri22@users.noreply.github.com>
2023-11-25 07:27:30 -0500

    Update setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install -y build-essential pkg-config python3 python3-venv python3-pip make git
-sudo apt-get install -y python3 tcsh tcl-dev tk-dev libcairo2-dev m4
+sudo apt-get install -y tcsh tcl-dev tk-dev libcairo2-dev m4
 sudo apt-get install vim
 sudo apt-get install valgrind
 sudo apt upgrade cmake gnuplot libglfw3-dev
