3ec62a07f81bfd83d1c5e5e6fd8aa8162b66ece9
Matthew Morrison <mmorri22@nd.edu>
2023-11-25 06:13:55 -0500

    Add script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt install -y build-essential pkg-config
+sudo apt-get install -y python3 tcsh tcl-dev tk-dev libcairo2-dev m4
+sudo apt-get install vim
+sudo apt-get install valgrind
+sudo apt-get install python3-venv
+sudo apt-get install python3-pip
+sudo apt install git
+sudo apt upgrade cmake gnuplot libglfw3-dev
+sudo apt-get install libfftw3-3
+sudo apt-get install libfftw3-dev
+sudo apt-get install xz-utils
